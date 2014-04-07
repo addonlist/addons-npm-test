601b72d02ba3e704c1e11224791e526964bf2743
Ben Pellow <benjamin.pellow@gmail.com>
2014-04-06 20:51:39 -0700

    server showing our stuff is working :) :panda_face:

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ var addons = require('addons');
 addons();
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  var vars = "<br />MAILGUN_API_KEY: " + process.env.MAILGUN_API_KEY;
+  vars += "<br />MONGOLAB_URI: " + process.env.MONGOLAB_URI;
+  vars += "<br />LAUNCHBOX_APP_ID: " + process.env.MAILGUN_API_KEY;
+  vars += "<br />ADDONS_APP_ID: " + process.env.MAILGUN_API_KEY;
+  vars += "<br />ADDONS_APP_TOKEN: " + process.env.MAILGUN_API_KEY;
+
+  res.send("ENVIRONMENT VARS:<br />" + vars);
 });
 
 app.listen(3000);
