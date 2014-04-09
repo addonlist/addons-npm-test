a74176f38ca0d3aad7c3c147ef99f2a427e607df
Ben Pellow <benjamin.pellow@gmail.com>
2014-04-08 21:20:23 -0700

    email form :turtle:

--- server.js
+++ server.js
@@ -5,13 +5,42 @@ var addons = require('addons');
 addons();
 
 app.get('/', function(req, res){
-  var vars = "<br />MAILGUN_API_KEY: " + process.env.MAILGUN_API_KEY;
+  var emailForm = "<body> \
+<h1>New email</h1> \
+<form accept-charset=\"UTF-8\" action=\"/emails\" class=\"new_email\" id=\"new_email\" method=\"post\"><div style=\"margin:0;padding:0;display:inline\"><input name=\"utf8\" type=\"hidden\" value=\"✓\"></div> \
+  <div class=\"field\"> \
+    <label for=\"email_to\">To</label><br> \
+    <input id=\"email_to\" name=\"email[to]\" type=\"text\"> \
+  </div> \
+  <div class=\"field\"> \
+    <label for=\"email_from\">From</label><br> \
+    <input id=\"email_from\" name=\"email[from]\" type=\"text\"> \
+  </div> \
+  <div class=\"field\"> \
+    <label for=\"email_subject\">Subject</label><br> \
+    <input id=\"email_subject\" name=\"email[subject]\" type=\"text\"> \
+  </div> \
+  <div class=\"field\"> \
+    <label for=\"email_body\">Body</label><br> \
+    <input id=\"email_body\" name=\"email[body]\" type=\"text\"> \
+  </div> \
+  <div class=\"actions\"> \
+    <input name=\"commit\" type=\"submit\" value=\"Create Email\"> \
+  </div> \
+</form> \
+</body>"
+
+  var vars = emailForm;
+  vars += "<footer>";
+  vars += "ENVIRONMENT VARS:<br />";
+  vars += "<br />MAILGUN_API_KEY: " + process.env.MAILGUN_API_KEY;
   vars += "<br />MONGOLAB_URI: " + process.env.MONGOLAB_URI;
   vars += "<br />LAUNCHBOX_APP_ID: " + process.env.MAILGUN_API_KEY;
   vars += "<br />ADDONS_APP_ID: " + process.env.MAILGUN_API_KEY;
   vars += "<br />ADDONS_APP_TOKEN: " + process.env.MAILGUN_API_KEY;
+  vars += "</footer>";
 
-  res.send("ENVIRONMENT VARS:<br />" + vars);
+  res.send(vars);
 });
 
 app.listen(process.env.PORT || 3000);
